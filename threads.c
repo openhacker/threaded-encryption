0dd517bcc638782ebdbb7e9cf5aeedecafd5a8dd
Marty Leisner <maleisner@gmail.com>
2024-02-27 23:40:05 -0500

    start doing copy_dev_zero

--- threads.c
+++ threads.c
@@ -23,13 +23,14 @@ struct thread_info {
 	struct timeval time_started;
 	pthread_mutex_t work_available;   // unlocked to start work , 
 	volatile bool done;		// work finished, unlock work_avilable to start new work cycle
+	bool terminated;		// thread is terminted
 	pthread_t thread_info;
 };
 
 
 
 static struct thread_info *each_thread;
-static int num_threads;
+static int num_threads = 1;
 
 static void safe_write(int fd, char *buffer, int size)
 {
@@ -74,9 +75,11 @@ static void *copy_file(void *args)
 				printf("EOF: bytes_copied = %d\n", bytes_copied);
 				break;
 			}
+			bytes_copied += bytes_read;
 		}	
 		p->done = true;
 		pthread_cond_broadcast(&cv);
+		fprintf(stderr, "%d ended work\n", (int) pthread_self());
 	}
 }
 
@@ -105,10 +108,10 @@ static void threads_zero_to_null(void)
 {
 	int i;
 	const size_t size = 1000 * 1000 * 1000;	
-	int number_of_work = 10;
+	int work_left = 100;
 	struct thread_info *pthread;
 
-	for(pthread = each_thread, i = 0; i < num_threads; i++, pthread++) {
+	for(pthread = each_thread; pthread  < each_thread + num_threads; pthread++) {
 		int result;
 
 		result = pthread_mutex_init(&pthread->work_available, NULL);
@@ -116,9 +119,9 @@ static void threads_zero_to_null(void)
 		result = pthread_mutex_lock(&pthread->work_available);
 		assert(result == 0);
 		pthread->input = open("/dev/zero", O_RDONLY);
-		assert(pthread->input < 0);
+		assert(pthread->input >= 0);
 		pthread->output = open("/dev/null", O_WRONLY);
-		assert(pthread->output < 0);
+		assert(pthread->output >= 0);
 		pthread->done = false;
 		pthread->bytes = size;
 		result = pthread_create(&pthread->thread_info, NULL, copy_file, pthread);
@@ -126,21 +129,52 @@ static void threads_zero_to_null(void)
 	}
 
 	pthread_mutex_lock(&able_to_condition);
-	number_of_work -= num_threads;	
+	work_left -= num_threads;	
 
 	for(pthread = each_thread; pthread < each_thread + num_threads; pthread++) {
 		pthread_mutex_unlock(&pthread->work_available);
 	}
 
 	while(1) {
+		printf("number of workers left = %d\n", work_left);
+
 		pthread_cond_wait(&cv, &able_to_condition);
 
-		while(1) {
+		for(pthread = each_thread; pthread < each_thread + num_threads; pthread++) {
+			if(pthread->done == true) {
+				close(pthread->input);
+				close(pthread->output);
+				if(!work_left) {
+					pthread->terminated = true;
+					pthread->done = false;
+					pthread_cancel(pthread->thread_info);
+				} else {
+					work_left--;
+					pthread->input = open("/dev/zero", O_RDONLY);
+					pthread->output = open("/dev/null", O_WRONLY);
+					pthread->done = false;
+					pthread->bytes = size;
+					pthread_mutex_unlock(&pthread->work_available);
+				}
+			}
+		}
+		
+		bool not_terminated = false;
+
+		/* if all threads terminated, exit */
+		for(pthread = each_thread; pthread < each_thread + num_threads; pthread++) {
+			if(pthread->terminated == false) {
+				not_terminated = true;
+				break;
+			} 
 		}
 
+		if(not_terminated == false) {
+			printf("work left = %d\n", work_left);	
+			exit(0);
+
+		}
 	}
-
-
 
 }
 				
