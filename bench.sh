552819fe52d3ac9f26b4f04128b9e97256aa4937
Marty Leisner <maleisner@gmail.com>
2024-06-23 11:38:00 -0400

    improved bench script

--- bench.sh
+++ bench.sh
@@ -1,25 +1,55 @@
 #! /bin/bash
 
-DIRECTORY=/usr/local/space/3000
+# DIRECTORY=/usr/local/space/3000
+
+# DIRECTORY=/media/leisner/encrypt-spac/100
+DIRECTORY=/media/leisner/encrypt-spac/3000
+
 
 export REPORT_SPEED=1
 set -e
 
-ls -l $DIRECTORY
+first=0
 
+echo 'zero_files (in core encrption)'
 for i in 1 2 3 4
 do
-	./zero_files $i
+	if [[ $first -gt 0 ]]; then
+		export NO_HEADING=1
+	fi
+	./zero_files -t $i
+	first=1
 done
 
+
+unset NO_HEADING
+echo
+
+first=0
+
+echo no write on filesystem
 for i in 1 2 3 4
 do
+	if [[ $first -gt 0 ]]; then
+		export NO_HEADING=1
+	fi
 	./encrypt_files -E -d $DIRECTORY -n -t $i
+	first=1
 done
 
+first=0
+unset NO_HEADING
+echo
+
+echo read/write encryption/decryption
 for i in 1 2 3 4
 do
 	./encrypt_files -E -d $DIRECTORY  -t $i
+	first=1
+	if [[ $first -gt 0 ]]; then
+		export NO_HEADING=1
+	fi
+
 	./encrypt_files -D -d $DIRECTORY -t $i
 done
 
